c7469749784357739420d41dac8b7e4b648a6ece
Ellie Benuska <elliebenuska@gmail.com>
2018-04-06 15:19:50 -0700

    commit2

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log("working")
+
+var selectedInput = null;
+$(document).ready(function() {
+    $('button').focus(function() {
+        this.blur();
+    });
+});
+
+
+$(".submit-button").click(function(event){
+    event.preventDefault();
+    $(".submit-button").css("background-color", "#e6e6e6");
+    console.log("working")
+});
+
